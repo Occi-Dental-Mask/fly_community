4afd81a0a8367f626ad9f1ab38da79fa134ee1dd
occi <935673287@qq.com>
2024-05-17 15:53:17 +0800

    "first commit"

---
+++ src/main/java/com/fly/community/service/UserService.java
@@ -0,0 +1,18 @@
+package com.fly.community.service;
+
+import com.fly.community.dao.UserMapper;
+import com.fly.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
+
+}
